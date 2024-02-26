38067f3bfe377532e403e2a22f3ed5af266655ef
JanakBB <janakcustomx@gmail.com>
2024-02-26 10:04:59 +0545

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -56,12 +56,14 @@ products.forEach((product) => {
     `;
 });
 
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
+        // console.log(button.dataset.productId);
         const productId = button.dataset.productId;
 
         let matchingItem;
@@ -80,7 +82,18 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity: 1
             });
         }
-        console.log(cart);
-    })
+
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+         .innerHTML = cartQuantity;
+
+        // console.log(cartQuantity);
+        // console.log(cart);
+    });
   });
 
